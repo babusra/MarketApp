df1c4b122bd3d99c7bb5145ff845d2e020da0cda
Büşra Arıgün <busrarigun@gmail.com>
2023-03-18 01:51:16 +0300

    ProductCard: props added and revises

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,50 +1,47 @@
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import React from 'react';
+import {IProduct} from '../models/ProductModel';
+import { horizontalScale, moderateScale, verticalScale } from '../constants/Dimension';
 
-const ProductCard = () => {
+interface Props {
+  product: IProduct;
+}
+
+const ProductCard = (props: Props) => {
+  const {product} = props;
+
   return (
-    <View
-      style={styles.container}>
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: 'lightgrey',
-          alignItems: 'center',
-          height: '50%',
-        }}>
-        <Text>Image</Text>
-      </View>
-      <Text style={styles.text_price}>15.000 ₺</Text>
-      <Text style={styles.text_productName}>iPhone 13 Pro Max 256Gb</Text>
+    <TouchableOpacity style={styles.container}>
+     
+        <Image style={{width:horizontalScale(100),height:verticalScale(100),alignSelf:'center'}} source={{uri:'https://placeimg.com/640/480/abstract'}}/>
+      <Text style={styles.text_price}>{product.price} ₺</Text>
+      <Text style={styles.text_productName}>{product.brand}</Text>
       <TouchableOpacity style={styles.button}>
-        <Text
-          style={styles.button_text}>
-          Add to Cart
-        </Text>
+        <Text style={styles.button_text}>Add to Cart</Text>
       </TouchableOpacity>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 export default ProductCard;
 const styles = StyleSheet.create({
-    container:{
-        borderWidth: 2,
-        borderColor: 'lightgrey',
-        borderRadius: 8,
-        height: '60%',
-        width: '40%',
-        padding: 10,
-        justifyContent: 'space-between',
-    },
-    text_price:{color: 'blue'},
-    text_productName:{fontSize: 12},
-    button:{backgroundColor: 'blue', borderRadius: 4},
-    button_text:{
-        color: '#fff',
-        fontSize: 12,
-        padding: 7,
-        textAlign: 'center',
-      }
-
+  container: {
+    borderWidth: 2,
+    borderColor: 'lightgrey',
+    borderRadius: 8,
+    height: verticalScale(250),
+    width: horizontalScale(150),
+    padding: 10,
+    justifyContent: 'space-between',
+    margin:moderateScale(10)
+  },
+  text_price: {color: 'blue'},
+  text_productName: {fontSize: 12},
+  button: {backgroundColor: 'blue', borderRadius: 4},
+  button_text: {
+    color: '#fff',
+    fontSize: 12,
+    padding: moderateScale(9),
+    textAlign: 'center',
+  },
 });
