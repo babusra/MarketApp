918f84254781dc5573741bc3f2d8ae2388105a1c
Büşra Arıgün <busrarigun@gmail.com>
2023-03-18 02:55:04 +0300

    ProductCard revised

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,32 @@
 import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import React from 'react';
 import {IProduct} from '../models/ProductModel';
-import { horizontalScale, moderateScale, verticalScale } from '../constants/Dimension';
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from '../constants/Dimension';
+import {Colors} from '../constants/Colors';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   product: IProduct;
 }
 
 const ProductCard = (props: Props) => {
+  const navigation = useNavigation();
   const {product} = props;
 
   return (
-    <TouchableOpacity style={styles.container}>
-     
-        <Image style={{width:horizontalScale(100),height:verticalScale(100),alignSelf:'center'}} source={{uri:'https://placeimg.com/640/480/abstract'}}/>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('ProductDetailScreen', {product: product})
+      }
+      style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{uri: 'https://placeimg.com/640/480/abstract'}}
+      />
       <Text style={styles.text_price}>{product.price} ₺</Text>
       <Text style={styles.text_productName}>{product.brand}</Text>
       <TouchableOpacity style={styles.button}>
@@ -33,11 +46,16 @@ const styles = StyleSheet.create({
     width: horizontalScale(150),
     padding: 10,
     justifyContent: 'space-between',
-    margin:moderateScale(10)
+    margin: moderateScale(10),
   },
-  text_price: {color: 'blue'},
+  image: {
+    width: horizontalScale(120),
+    height: verticalScale(120),
+    alignSelf: 'center',
+  },
+  text_price: {color: Colors.blue},
   text_productName: {fontSize: 12},
-  button: {backgroundColor: 'blue', borderRadius: 4},
+  button: {backgroundColor: Colors.blue, borderRadius: 4},
   button_text: {
     color: '#fff',
     fontSize: 12,
